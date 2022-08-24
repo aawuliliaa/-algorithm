0ee9912e57421c1d557c8118818a340aa7425ff4
wulili <wulili@bixin.cn>
2022-08-24 15:13:44 +0800

    wulili

--- stackToQueue/main.go
+++ stackToQueue/main.go
@@ -9,7 +9,7 @@ type stack struct {
 
 func newStack(cp int) *stack {
 	return &stack{
-		data: make([]interface{}, 0,cp),
+		data: make([]interface{}, 0, cp),
 		size: 0,
 	}
 }
@@ -22,8 +22,7 @@ func (s *stack) Pop() interface{} {
 		panic("stack is empty")
 	}
 	e := s.data[s.size-1]
-
-	s.data[s.size-1] = nil
+	s.data = s.data[:s.size-1]
 	s.size--
 	return e
 }
@@ -33,13 +32,75 @@ func (s *stack) Size() int {
 func (s *stack) IsEmpty() bool {
 	return s.size == 0
 }
+
+type MyQueue struct {
+	s1 *stack
+	s2 *stack
+}
+
+func Constructor() MyQueue {
+	return MyQueue{s1: newStack(3), s2: newStack(3)}
+}
+
+func (this *MyQueue) Push(x int) {
+	this.s1.Push(x)
+}
+
+//从队列开头移除并返回
+func (this *MyQueue) Pop() int {
+	if this.s1.IsEmpty() {
+		panic("empty queue")
+	}
+	if this.s1.size == 1 {
+		return this.s1.Pop().(int)
+	}
+	//先挪到s2
+	s1Size := this.s1.size
+	for i := 1; i < s1Size; i++ {
+		this.s2.Push(this.s1.Pop())
+	}
+	//获取s1中的最底下的元素，返回
+	e := this.s1.Pop().(int)
+	//再把s2中的元素挪回到s1
+	s2Size := this.s2.size
+	for i := 1; i <= s2Size; i++ {
+		this.s1.Push(this.s2.Pop())
+	}
+	return e
+}
+
+//返回队列开头的元素
+func (this *MyQueue) Peek() int {
+	s1Size := this.s1.size
+	for i := 1; i < s1Size; i++ {
+		this.s2.Push(this.s1.Pop())
+	}
+	//获取s1中的最底下的元素，返回
+	e := this.s1.Pop().(int)
+	this.s1.Push(e)
+	//再把s2中的元素挪回到s1
+	s2Size := this.s2.size
+	for i := 1; i <= s2Size; i++ {
+		this.s1.Push(this.s2.Pop())
+	}
+	return e
+
+}
+
+func (this *MyQueue) Empty() bool {
+	return this.s1.size == 0
+}
+
 func main() {
-	s := newStack(3)
-	for _,i :=range []int{1,2,3,4,5,6,7}{
-		s.Push(i)
+
+	q := Constructor()
+
+	for _, i := range []int{1, 2, 3, 4, 5, 6, 7} {
+		q.Push(i)
 	}
-	fmt.Println(s)
-	for i:=0;i<3;i++{
-		fmt.Println(s.Pop())
+	for i := 0; i < 3; i++ {
+		fmt.Println(q.Pop())
 	}
+	fmt.Println(q.Peek())
+
 }
