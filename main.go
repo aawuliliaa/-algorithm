e148c3a95a2b3b340b2973230d96245619d3c703
wulili <wulili@192.168.0.104>
2022-09-02 22:45:02 +0800

    wulili

---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func uniqueMorseRepresentations(words []string) int {
+	code := []string{".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---", "-.-", ".-..", "--", "-.", "---", ".--.", "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--.."}
+	wordCodeMap :=make(map[string]int)
+	for _, word := range words {
+		wordCode := ""
+		for _,i :=range word{
+			wordCode += code[i-97]
+		}
+		wordCodeMap[wordCode] =1
+	}
+	return len(wordCodeMap)
+}
+func main() {
+	words := []string{"gin", "zen", "gig", "msg"}
+	fmt.Println(uniqueMorseRepresentations(words))
+
+}
