c18ab440f3bcf7e72f4205eb4733b653b3ffda3d
wulili <wulili@192.168.0.104>
2022-09-02 09:52:07 +0800

    wulili

--- binarySearch/contains/main.go
+++ binarySearch/contains/main.go
@@ -17,19 +17,35 @@ func NewTree() *tree {
 }
 
 func (t *tree) add(e int) {
-	t.root = t.addR(t.root,e)
+	t.root = t.addR(t.root, e)
 
 }
 func (t *tree) addR(node *Node, e int) *Node {
 	if node == nil {
 		t.size++
-		return &Node{e:e}
-	}else if node.e>e{
-		node.left = t.addR(node.left,e)
-	}else {
-		node.right = t.addR(node.right,e)
+		return &Node{e: e}
+	} else if node.e > e {
+		node.left = t.addR(node.left, e)
+	} else {
+		node.right = t.addR(node.right, e)
 	}
 	return node
+}
+func (t *tree) contains(e int) bool {
+	return t.containsR(t.root,e)
+
+}
+func (t *tree) containsR(node *Node, e int) bool {
+	if node==nil{
+		return false
+	}
+	if node.e==e{
+		return true
+	}else if node.e>e{
+		return t.containsR(node.left,e)
+	}else{
+		return t.containsR(node.right,e)
+	}
 }
 
 func main() {
@@ -37,6 +53,6 @@ func main() {
 	for _, i := range []int{3, 5, 1, 2, 4} {
 		tree.add(i)
 	}
-	fmt.Println(tree)
+	fmt.Println(tree.contains(6))
 
 }
